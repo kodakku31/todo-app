723b2afa25c5082ee5d5aae9bdca44fa47001065
kodakku31 <yamakotakota@icloud.com>
2025-02-09 12:03:02 +0900

    Fix PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -53,19 +53,19 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: '/icons/todo-icon.svg',
+            src: '/icons/todo-icon-192x192.svg',
             sizes: '192x192',
             type: 'image/svg+xml',
             purpose: 'any'
           },
           {
-            src: '/icons/todo-icon.svg',
+            src: '/icons/todo-icon-512x512.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'any'
           },
           {
-            src: '/icons/todo-icon.svg',
+            src: '/icons/todo-icon-512x512-maskable.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'maskable'
